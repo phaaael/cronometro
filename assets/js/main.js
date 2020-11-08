b4131fceb62e04d8b72c108c41ee167bc81d2e36
G0RD4LH4 <gordalha77@gmail.com>
2020-11-08 00:48:56 -0300

    ...

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+function clock() {
+  function getTimeFromSeconds(seconds) {
+    const date = new Date(seconds * 1000);
+    return date.toLocaleTimeString('pt-BR', { hour12: false, timeZone: 'UTC' });
+  }
+
+  const clock = document.querySelector('.clock');
+  let seconds = 0;
+  let timer;
+
+  function startClock() {
+    timer = setInterval(function() {
+      seconds++;
+      clock.innerHTML = getTimeFromSeconds(seconds);
+    }, 1000);
+  }
+
+  document.addEventListener('click', function(event) {
+    const el = event.target;
+    console.log(el)
+
+    if (el.classList.contains('reset')) {
+      clearInterval(timer);
+      clock.innerHTML = '00:00:00';
+      clock.classList.remove('paused');
+      seconds = 0;
+    }
+
+    if (el.classList.contains('start')) {
+      clock.classList.remove('paused');
+      clearInterval(timer);
+      startClock();
+    }
+
+    if (el.classList.contains('stop')) {
+      clearInterval(timer);
+      clock.classList.add('paused');
+    }
+
+    if(el.classList.contains('fiveMore')) {
+      if(clock.innerHTML = '00:00:00') {
+        clock.innerHTML = getTimeFromSeconds(seconds + 5)
+      }
+      seconds = seconds + 5
+    }
+
+    if(el.classList.contains('fifteenMore')) {
+      if(clock.innerHTML = '00:00:00') {
+        clock.innerHTML = getTimeFromSeconds(seconds + 15)
+      }
+      seconds = seconds + 15
+    }
+
+    if(el.classList.contains('thirtyMore')) {
+      if(clock.innerHTML = '00:00:00') {
+        clock.innerHTML = getTimeFromSeconds(seconds + 30)
+      }
+      seconds = seconds + 30
+    }
+  });
+}
+
+clock();
